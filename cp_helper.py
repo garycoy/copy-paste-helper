639cd11a5d0b22302d54bd5b3e72bc0e81fb69e4
Gary Coy <32619147+garycoy@users.noreply.github.com>
2025-09-02 16:13:25 -0500

    Update cp_helper.py
    add logging, fixed bug around using : as a delimiter, switched to os.system call, changed 'command file' to a parameter instead of hardcoded 'commands.txt'

--- cp_helper.py
+++ cp_helper.py
@@ -2,26 +2,35 @@
 
 # Reference: https://github.com/garycoy/copy-paste-helper/tree/main
 
-import subprocess
-import sys
+# DO NOT CALL THIS DIRECTLY. It's meant to be called from
+# cph or rc.
 
-# First and only argument to cp_helper.py is the id of target GNU screen session
-# This MUST be correct, and must be present
+import sys
+import os
+import time
+
 screen_sess = sys.argv[1]
 
-commands = open("./commands.txt").read().splitlines()
+command_file = sys.argv[2]
+
+commands = open(f'./{command_file}').read().splitlines()
 for one_command in commands:
-    final_command = one_command.split(":")[1]
     if one_command.startswith("COMMAND:"):
-        subprocess.call(["screen", "-S", screen_sess, "-X", "stuff", final_command])
+        final_command = one_command.replace("COMMAND:","")
+        final_command = "$'" + final_command + "'"
+        os.system("screen -S " + screen_sess + " -X stuff " + final_command)
         # last_action = "COMMAND"
     if one_command.startswith("RETURN:"):
-        # last_action = "RETURN"
-        if final_command == "ASK":
+        return_command = one_command.replace("RETURN:","")
+        if return_command == "ASK":
             yes_no = input("Do you want to type the enter key [y/n]?: ")
             if yes_no == "":
                 yes_no = "y"
             if yes_no == "y":
-                subprocess.call(["screen", "-S", screen_sess, "-X", "stuff", "\015"])
+                os.system("screen -S " + screen_sess + " -X stuff \015" )
+                with open("./cp_helper.log","a") as logfile:
+                  logfile.write(time.strftime("%Y-%m-%d %H:%M:%S") + " => Executed: " + final_command + "\n")
         else:
-            subprocess.call(["screen", "-S", screen_sess, "-X", "stuff", "\015"])
+            os.system("screen -S " + screen_sess + " -X stuff \015" )
+            with open("./cp_helper.log","a") as logfile:
+               logfile.write(time.strftime("%Y-%m-%d %H:%M:%S") + " => Executed: " + final_command + "\n")
